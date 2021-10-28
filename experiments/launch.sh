738370558f8584e285407cc924837f587c8c0b12
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2021-10-27 20:12:31 -0400

    Config files for early experiments

---
+++ experiments/launch.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# echo 'EXP 01'
+# python3 train.py --algo a2c --yaml ex01.yaml --steps 100000
+
+# echo 'EXP 02'
+# python3 train.py --algo a2c --yaml ex02.yaml --steps 100000
+
+# echo 'EXP 03'
+# python3 train.py --algo a2c --yaml ex03.yaml --steps 100000
+
+# echo 'EXP 04'
+# python3 train.py --algo a2c --yaml ex04.yaml --steps 100000
+
+echo 'EXP 05'
+python3 train.py --algo ppo --yaml ex01.yaml --steps 100000
+
+# echo 'EXP 06'
+# python3 train.py --algo ppo --yaml ex02.yaml --steps 100000
+
+# echo 'EXP 07'
+# python3 train.py --algo ppo --yaml ex03.yaml --steps 100000
+
+# echo 'EXP 08'
+# python3 train.py --algo ppo --yaml ex04.yaml --steps 100000
+
+# echo 'EXP 09'
+# python3 train.py --algo a2c --yaml ex09.yaml --steps 100000
+
+# echo 'EXP 010'
+# python3 train.py --algo a2c --yaml ex10.yaml --steps 100000
+
+# echo 'EXP 011'
+# python3 train.py --algo a2c --yaml ex11.yaml --steps 100000
+
+# echo 'EXP 012'
+# python3 train.py --algo a2c --yaml ex12.yaml --steps 100000
+
+# echo 'EXP 013'
+# python3 train.py --algo a2c --yaml ex13.yaml --steps 100000
+
+# echo 'EXP 014'
+# python3 train.py --algo a2c --yaml ex14.yaml --steps 100000
+
+# echo 'EXP 015'
+# python3 train.py --algo ppo --yaml ex09.yaml --steps 100000
+
+# echo 'EXP 016'
+# python3 train.py --algo ppo --yaml ex10.yaml --steps 100000
+
+# echo 'EXP 017'
+# python3 train.py --algo ppo --yaml ex11.yaml --steps 100000
+
+# echo 'EXP 018'
+# python3 train.py --algo ppo --yaml ex12.yaml --steps 100000
+
+# echo 'EXP 019'
+# python3 train.py --algo ppo --yaml ex13.yaml --steps 100000
+
+# echo 'EXP 020'
+# python3 train.py --algo ppo --yaml ex14.yaml --steps 100000
